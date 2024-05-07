da2959d5976c93bad16bebf60cb7408683fb8ac5
ClaraJGit <clarabellejaquet@yahoo.fr>
2024-05-07 17:07:14 +0200

    deleting a useless reference  module file and adding up missing file

---
+++ organelles_classes/nucleus.py
@@ -0,0 +1,5 @@
+class Nucleus:
+    def __init__(self, ):
+        self.area = 0  # Nucleus area
+        self.min_diameter = 0  # Nucleus minimum diameter
+        self.max_diameter = 0  # Nucleus maximum diameter
